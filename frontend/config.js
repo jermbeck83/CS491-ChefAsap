a210384d42c10b5f372b7f1b7090f3c4a334be4d
jjani11 <johnoaj11@gmail.com>
2026-05-02 16:08:43 -0400

    Forgot to change ApiURL

--- frontend/config.js
+++ frontend/config.js
@@ -1,8 +1,8 @@
 import { Platform } from 'react-native';
 
 const getEnvVars = () => ({
-  //apiUrl: 'https://chefasap-backend.onrender.com',
-  apiUrl: 'http://192.168.1.152:3000',
+  apiUrl: 'https://chefasap-backend.onrender.com',
+  
 });
 
 export default getEnvVars;
